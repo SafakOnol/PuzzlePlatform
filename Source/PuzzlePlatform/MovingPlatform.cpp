3df10df97319734720c94c01e8f34e96c45dd386
SafakOnol <onolsafak@outlook.com>
2022-08-07 10:00:32 -0400

    Initial Commit

---
+++ Source/PuzzlePlatform/MovingPlatform.cpp
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingPlatform.h"
+
+AMovingPlatform::AMovingPlatform()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	SetMobility(EComponentMobility::Movable);
+}
+
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (HasAuthority())
+	{
+		SetReplicates(true);
+		SetReplicateMovement(true);
+	}
+	
+	GlobalStartLocation = GetActorLocation();
+	GlobalTargetLocation = GetTransform().TransformPosition(TargetLocation);
+}
+
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (HasAuthority())
+	{
+		FVector Location = GetActorLocation();
+
+		// get magnitudes of initial travel distance projection and actual travelling distance
+		// when actual travel reaches or passes the initial projection, turn back
+		float JourneyLength = (GlobalTargetLocation - GlobalStartLocation).Size(); // Size() = magnitude of Vector
+		float JourneyTravelled = (Location - GlobalStartLocation).Size(); //
+
+		if (JourneyTravelled >= JourneyLength)
+		{
+			FVector Swap = GlobalStartLocation;
+			GlobalStartLocation = GlobalTargetLocation;
+			GlobalTargetLocation = Swap;
+		}
+
+		FVector Direction = (GlobalTargetLocation - GlobalStartLocation).GetSafeNormal();
+		Location += Speed * DeltaTime * Direction;
+		SetActorLocation(Location);
+	}
+	
+	
+	
+}
