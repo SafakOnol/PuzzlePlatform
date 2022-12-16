b871abc4f8bde6fb4f23d1d2fa44efb5e9c61665
SafakOnol <onolsafak@outlook.com>
2022-12-16 10:36:55 -0500

    misc

---
+++ Source/PuzzlePlatform/ItemSpawner.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/TargetPoint.h"
+#include "ItemSpawner.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEPLATFORM_API AItemSpawner : public ATargetPoint
+{
+	GENERATED_BODY()
+	
+};
